3ec99eb09809f764e2a5edbbec6810f36d343a52
Chris Reudenbach <reudenbach@uni-marburg.de>
2022-01-26 23:11:19 +0100

    fix mixed up variables names

--- src/main2.R
+++ src/main2.R
@@ -17,9 +17,12 @@
 library(envimaR)
 library(rprojroot)
 library(sen2r)
-#--- Schalter für Download
+#--- Schalter für den Download der sentinel daten
 get_sen = FALSE
 
+#--- schalter ob digitalisiert werden muss falls er auf FALSE gesetzt ist werden die
+# (zuvor erstellten und gesciherten Daten ) im else Teil der Verzeigung eingelesen
+digitize = FALSE
 
 ## setzen des aktuellen Projektverzeichnisses (erstellt mit envimaR) als root_folder
 #root_folder = find_rstudio_root_file()
@@ -61,15 +64,14 @@ names(pred_stack_2020) = c("nir","red","green","red","green","blue","EVI","MSAVI
 prediction_kmeans_2019 = unsuperClass(pred_stack_2019, nClasses = 2,norm = TRUE, algorithm = "MacQueen" )
 prediction_kmeans_2020 = unsuperClass(pred_stack_2020, nClasses = 2,norm = TRUE, algorithm = "MacQueen")
 
-# Visualisierung
+# Visualisierung Note mit dem + Zeichen können beliebige raster und vektor  Datenebnenen kombiniert werden
 mapview(prediction_kmeans_2019$map,col.regions = mapviewPalette("mapviewTopoColors"), at = seq(0, 2, 1), legend = TRUE,alpha.regions = 0.5) +
 mapview(prediction_kmeans_2020$map,col.regions = mapviewPalette("mapviewTopoColors"), at = seq(0, 2, 1), legend = FALSE,alpha.regions = 0.5)
 
 
-
-
 #---- Digitalisierung der Trainingsdaten ----
 
+if (digitize) {
 # Für die überwachte Klassifikation benötigen wir Trainingsgebiete. Sie können Sie wie nachfolgend digitalisieren oder alternativ z.B. QGis verwenden
 
 #--- 2019
@@ -81,48 +83,51 @@ train_area <- mapview::viewRGB(pred_stack_2019, r = 1, g = 2, b = 3, maxpixels =
 # Hinzufügen der Attribute class (text) und id (integer)
 clearcut <- train_area$finished$geometry %>% st_sf() %>% mutate(class = "clearcut", id = 1)
 
-# kein Wald: hier gilt es möglichst verteilt übers Bild möglichst alle nicht zu Kahlschlag  gehörenden Flächen zu erfassen.
+# other: hier gilt es möglichst verteilt übers Bild möglichst alle nicht zu Kahlschlag  gehörenden Flächen zu erfassen.
 train_area <- mapview::viewRGB(pred_stack_2019, r = 1, g = 2, b = 3) %>% mapedit::editMap()
 other <- train_area$finished$geometry %>% st_sf() %>% mutate(class = "other", id = 2)
 
 # rbind  kopiert die beiden obigen Vektorobjekte in eine Datei
 train_areas_2019 <- rbind(clearcut, other)
 
-# Abspeichern als R-internes Datenformat
-#saveRDS(train_areas_2019, paste0(envrmt$path_data,"train_areas_2019.rds"))
-train_areas_2019 = readRDS(paste0(envrmt$path_data,"train_areas_2019.rds"))
+# Umprojizieren auf die Raster Datei
+train_areas_2019 = sf::st_transform(train_areas_2019,crs = sf::st_crs(pred_stack_2019))
 
-#--- Das gleiche muss für 2020 wiederholt werden
-# Kahlschlag
-train_area <- mapview::viewRGB(pred_stack_2020, r = 1, g = 2, b = 3,maxpixels =  1693870) %>% mapedit::editMap()
-# Hinzufügen der Attribute class (text) und id (integer)
-clearcut <- train_area$finished$geometry %>% st_sf() %>% mutate(class = "clearcut", id = 1)
-# kein Wald
-train_area <- mapview::viewRGB(pred_stack_2020, r = 1, g = 2, b = 3) %>% mapedit::editMap()
-other <- train_area$finished$geometry %>% st_sf() %>% mutate(class = "other", id = 2)
-# merge zu einer Datei
-train_areas_2020 <- rbind(clearcut, other)
-# sichern
-saveRDS(train_areas_2020, paste0(envrmt$path_data,"train_areas_2020.rds"))
-
-#--- 2019 Umprojizieren entsprechend der Raster Datei
-tp_2019 = sf::st_transform(train_areas_2019,crs = sf::st_crs(pred_stack))
 # Extraktion der Trainingsdaten für die digitalisierten Flächen
 tDF_2019 = exactextractr::exact_extract(pred_stack_2019, train_areas_2019,  force_df = TRUE,
-                                   include_cell = TRUE,include_xy = TRUE,full_colnames = TRUE,include_cols = "class")
+                                        include_cell = TRUE,include_xy = TRUE,full_colnames = TRUE,include_cols = "class")
 #  auch hier wieder zusamenkopieren in eine Datei
 tDF_2019 = dplyr::bind_rows(tDF_2019)
 # Löschen von etwaigen Zeilen die NA (no data) Werte enthalten
 tDF_2019 = tDF_2019[  rowSums(is.na(tDF_2019)) == 0,]
 
-#--- 2020
-tp_2020 = sf::st_transform(train_areas_2020,crs = sf::st_crs(pred_stack))
+# check der extrahierten Daten
+tDF_2019
+
+# Abspeichern als R-internes Datenformat
+# ist im Repo hinterlegt und kann desahlb (zeile drunter) eingeladen werden
+saveRDS(tDF_2019, paste0(envrmt$path_data,"train_areas_2019.rds"))
+
+
+#--- Das gleiche muss für 2020 wiederholt werden
+# Kahlschlag
+train_area <- mapview::viewRGB(pred_stack_2020, r = 1, g = 2, b = 3,maxpixels =  1693870) %>% mapedit::editMap()
+clearcut <- train_area$finished$geometry %>% st_sf() %>% mutate(class = "clearcut", id = 1)
+train_area <- mapview::viewRGB(pred_stack_2020, r = 1, g = 2, b = 3) %>% mapedit::editMap()
+other <- train_area$finished$geometry %>% st_sf() %>% mutate(class = "other", id = 2)
+train_areas_2020 <- rbind(clearcut, other)
+train_areas_2020 = sf::st_transform(train_areas_2020,crs = sf::st_crs(pred_stack_2020))
 tDF_2020 = exactextractr::exact_extract(pred_stack_2020, train_areas_2020,  force_df = TRUE,
                                         include_cell = TRUE,include_xy = TRUE,full_colnames = TRUE,include_cols = "class")
 tDF_2020 = dplyr::bind_rows(tDF_2020)
 tDF_2020 = tDF_2020[  rowSums(is.na(tDF_2020)) == 0,]
+saveRDS(tDF_2020, paste0(envrmt$path_data,"train_areas_2020.rds"))
 
+} else {
+  train_areas_2019 = readRDS(paste0(envrmt$path_data,"train_areas_2019.rds"))
+  train_areas_2020 = readRDS(paste0(envrmt$path_data,"train_areas_2020.rds"))
 
+}
 
 ## ---- Überwachte  mit Klassifikation Random Forest ----
 
@@ -154,7 +159,7 @@ stopCluster(cl) # stopp Parallellisierung
 
 # Klassifikation wird häufig auch Vorhersage genannt.
 prediction_rf_2019  = predict(pred_stack_2019 ,cv_model_2019, progress = "text")
-mapview(prediction_rf_2019,col.regions = c('darkgreen', 'burlywood', 'green'))
+mapview(prediction_rf_2019,col.regions = mapviewPalette("mapviewTopoColors"), at = seq(0, 2, 1), legend = TRUE,alpha.regions = 0.5)
 
 
 
