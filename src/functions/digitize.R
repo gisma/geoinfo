49701faa2dc02b98aea4a26658723a127c688ff2
Chris Reudenbach <reudenbach@uni-marburg.de>
2022-02-12 22:32:20 +0100

    cleanup

--- src/functions/digitize.R
+++ src/functions/digitize.R
@@ -1,15 +1,23 @@
-# Für die überwachte Klassifikation benötigen wir Trainingsgebiete. Sie können Sie wie nachfolgend digitalisieren oder alternativ z.B. QGis verwenden
+# Für die überwachte Klassifikation benötigen wir Trainingsgebiete.
+# Sie können Sie wie nachfolgend digitalisieren oder alternativ z.B. QGis verwenden
+# Dieses Skript wird in das change_detection_workflow.R Skript eingelesen
+# falls digitize = TRUE definiert ist
+
 
 #--- 2019
 # Kahlschlag
-# Es wird das Falschfarbenkomosit in originaler Auflösung genutzt (maxpixels =  1693870)
-# Bitte beachten Sie dass es (1) deutlich länger lädt und (2) Vegetation in Rot dargestellt wird.
-# Die Kahlschäge sind jetzt grün
+# Es wird das Falschfarbenkomposit in originaler Auflösung genutzt (maxpixels =  1693870)
+# Bitte beachten Sie dass es in voller Auflösung (1) deutlich länger lädt und
+# (2) Vegetation in Rot dargestellt wird. Sollten sie Echtfarben bevorzugen
+# nutzen sie die Kanäle r=4, g=5, b=6
+
+# Kahlschläge/Trockenschäden etc.
 train_area <- mapview::viewRGB(pred_stack_2019, r = 1, g = 2, b = 3, maxpixels =  1693870) %>% mapedit::editMap()
 # Hinzufügen der Attribute class (text) und id (integer)
 clearcut <- train_area$finished$geometry %>% st_sf() %>% mutate(class = "clearcut", id = 1)
 
-# other: hier gilt es möglichst verteilt übers Bild möglichst alle nicht zu Kahlschlag  gehörenden Flächen zu erfassen.
+# other: hier gilt es möglichst gut verteilt übers Bild
+# alle nicht zu Kahlschlag  gehörenden Flächen zu erfassen
 train_area <- mapview::viewRGB(pred_stack_2019, r = 1, g = 2, b = 3) %>% mapedit::editMap()
 other <- train_area$finished$geometry %>% st_sf() %>% mutate(class = "other", id = 2)
 
@@ -23,7 +31,7 @@ train_areas_2019 = sf::st_transform(train_areas_2019,crs = sf::st_crs(pred_stack
 # Extraktion der Trainingsdaten für die digitalisierten Flächen
 tDF_2019 = exactextractr::exact_extract(pred_stack_2019, train_areas_2019,  force_df = TRUE,
                                         include_cell = TRUE,include_xy = TRUE,full_colnames = TRUE,include_cols = "class")
-#  auch hier wieder zusamenkopieren in eine Datei
+# auch hier wieder merge in eine Datei
 tDF_2019 = dplyr::bind_rows(tDF_2019)
 
 # Löschen von etwaigen Zeilen die NA (no data) Werte enthalten
@@ -39,9 +47,7 @@ mapview(train_areas_2019)+pred_stack_2019[[1]]
 saveRDS(tDF_2019, paste0(envrmt$path_data,"train_areas_2019.rds"))
 
 
-# # ---- Das gleiche muss für 2020 wiederholt werden zum digitalisieren und extrahieren bitte ent-kommentieren ----
-
-# # Kahlschlag
+# ---- Das gleiche muss für 2020 wiederholt werden
 train_area <- mapview::viewRGB(pred_stack_2020, r = 4, g =5, b = 6,maxpixels =  1693870) %>% mapedit::editMap()
 clearcut <- train_area$finished$geometry %>% st_sf() %>% mutate(class = "clearcut", id = 1)
 train_area <- mapview::viewRGB(pred_stack_2020, r = 4, g = 5, b = 6) %>% mapedit::editMap()
